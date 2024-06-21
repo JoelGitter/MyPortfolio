de70cdfd3183560f013d4f38c61b4546ea5c6e97
JoelGitter <joelrhenry99@gmail.com>
2024-06-21 15:26:06 -0700

    Fixing about navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Projects from "./components/Projects";
 import About from "./components/About";
 import SideNav from "./components/SideNav";
@@ -14,7 +19,7 @@ function App() {
         </div>
         <SideNav />
         <Routes>
-          <Route exact path="/" element={<About />} />
+          <Route path="*" element={<Navigate to="/about" />} />
           <Route path="/about" element={<About />} />
           <Route exact path="/projects" element={<Projects />} />
         </Routes>
