626c2752b91c827b8a18454856cf86c18843118d
JoelGitter <joelrhenry99@gmail.com>
2024-07-02 10:37:17 -0700

    Adding more info to the Markt page

--- src/components/MarktPOS/MarktPOS.js
+++ src/components/MarktPOS/MarktPOS.js
@@ -1,4 +1,7 @@
 import React from "react";
+import dashboard from "../../assets/dashboard.png";
+import volumeTrends from "../../assets/volume-trends.png";
+import "./MarktPOS.scss";
 
 function MarktPOS() {
   return (
@@ -14,14 +17,13 @@ function MarktPOS() {
         success of the product, we were acuired by Quilt Software in 2022. We
         have now grown to have 100s of stores all across America and millions in
         ARR.
+        <img className="product-screenshot" src={dashboard} />
         <br />
+        There is a highly customizable checkstand system paired with state of
+        the art reporting, which gives store owners the ability to maximize
+        profits and keep a close eye on things when they are away.
         <br />
-        During my tenure I have:
-        <br /> • Grown the team to 4 software engineers and 1 QA engineer.
-        <br /> • Completed many integrations (Quickbooks, Twilio, ECommerce,
-        Zapier)
-        <br /> • Redesigned the UI, which led to the T.V. Show Supermarket Sweep
-        using our system
+        <img className="product-screenshot" src={volumeTrends} />
       </div>
     </div>
   );
